39666e78d918d72ad1afacdd392c70d7caabe469
Nattie C. <47332368+nattiechan@users.noreply.github.com>
2025-02-04 20:46:55 -0800

    feat: add tests

--- test/aws-cdk.test.ts
+++ test/aws-cdk.test.ts
@@ -1,17 +1,32 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCdk from '../lib/aws-cdk-stack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { ECRStack } from '../lib/ecr-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsCdk.AwsCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+// Test assertion overview:
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.assertions-readme.html
+describe('CDK tests', () => {
+  const testEnv = {
+    account: 'some-account',
+    region: 'some-region',
+  };
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // how to print out the template to help with writing the tests
+  // console.log(JSON.stringify(template.toJSON(), null, 2));
+
+  test('ECR template has correct resources', () => {
+    const app = new App();
+    const stack = new ECRStack(app, 'TestECRStack', { env: testEnv });
+
+    // Template is a suite of assertions that can be run on a CDK stack
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.assertions.Template.html
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::ECR::Repository', {
+      ImageScanningConfiguration: { ScanOnPush: true },
+      RepositoryName: 'server-repository',
+    });
+    template.hasResource('AWS::ECR::Repository', {
+      UpdateReplacePolicy: 'Delete',
+      DeletionPolicy: 'Delete',
+    });
+  });
 });
