7e0132089f517275f6dd2cbba18257e1a22f3f93
Nattie C. <47332368+nattiechan@users.noreply.github.com>
2025-02-17 02:12:14 -0800

    feat: add index.ts for lib folder
    This way we simply the import
    for other files

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from './bastion-stack';
+export * from './deployment-stack';
+export * from './docker-image-stack';
+export * from './ecr-stack';
+export * from './rds-stack';
+export * from './vpc-stack';
+export * from './utils';
